c2f505b2c9571faec85f671329a4391d605fb355
Maino24 <mg02.gabriele@gmail.com>
2022-10-25 23:24:54 +0200

    creato ciclo for

---
+++ Snack2/main.js
@@ -0,0 +1,26 @@
+/*
+Generatore di “nomi cognomi” casuali del grande gatsby:
+Abbiamo una lista di nomi e una lista di cognomi, e da queste vogliamo generare una "falsa" lista di invitati,
+ dove ogni invitato nella lista, è l'unione di un "nome" e un "cognome" delle liste precedentemente dette, scelti in modo casuale.*/
+
+//creo un array per il nome e uno per i cognomi
+ const nomi = [`gabriele`, `giorgio`, `marco`, `alessandro`, `michele`]
+ const cognomi = [`rossi`, `bianchi`, `verdi`, `ferrari`, `motta`]
+
+ //creo una funzione che mi permette di associare un nome con un cognome
+ 
+ function genera(){
+    
+    //let nomiCognomi = ``
+
+    for (i = 0; i < nomi.length; i++){
+
+        if(){
+        nomiCognomi= nomi + cognomi
+    
+    console.log(nomiCognomi[i])
+
+    document.getElementById(`output`).innerHTML= `${nomiCognomi}`
+        }
+}
+ }
---
+++ Snack3/main.js
@@ -0,0 +1,28 @@
+//Crea un array di numeri interi e fai la somma di tutti gli elementi  che sono in posizione (indice) dispari
+
+// creo costante array
+const arrayNumeri = [55,1005, 5, 1000, 44, 0]
+
+console.log(arrayNumeri)
+
+//creo una varibaile somma e la pongo con valore 0
+let sommaNumeri 
+sommaNumeri = 0 
+
+
+
+//creo un ciclo for
+for (i = 0; i < arrayNumeri.length; i++){
+
+    if (arrayNumeri[i] % 2 !== 0 ){
+        sommaNumeri +=  arrayNumeri[i]
+        
+
+    }else{
+
+    }
+
+    console.log(sommaNumeri)
+    document.getElementById(`sommaNumeriDispari`).innerHTML= `${sommaNumeri}`
+}
+
